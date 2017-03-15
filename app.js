df480cbe58791e67271ee638d6579cbe4770ceb7
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2017-03-15 21:36:32 +0800

    update

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var app = express();
 var os = require("os");
 
 
+
 app.get("/", function(req,res) {
-	res.send({ip: req.connection.remoteAddress, os: os.hostname()});
-	console.log(req.connection.remoteAddress);
-	console.log(os.hostname());
+	var host = String(os.hostname());
+	var platform = String(os.platform());
+
+	res.send({ip: req.connection.remoteAddress, os: host + " " + platform});	
 });
 
 app.listen(process.env.PORT || 3000, function() {
